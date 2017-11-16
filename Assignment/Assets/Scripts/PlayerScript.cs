7bff19aa79911e62cb75cc59e5a49fbdd8d70da2
Thomas-Field <31514428+Thomas-Field@users.noreply.github.com>
2017-11-16 15:50:16 -0500

    Fixed Jump

--- Assignment/Assets/Scripts/PlayerScript.cs
+++ Assignment/Assets/Scripts/PlayerScript.cs
@@ -1,49 +1,54 @@
 ﻿using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
-    
+public class PlayerScript : MonoBehaviour
+{
+
 	public float speed = 10f;
 	Animator my_animator;
 	Rigidbody2D rb;
 	[Range(1, 20)]
 	public float jumpVelocity;
-	bool isGrounded = true;
+	private bool isTouchingGround;
+	public Transform groundCheckPoint;
+	public float groundCheckRadius;
+	public LayerMask groundLayer;
 
-	void Start () {
-		my_animator = GetComponent<Animator> ();
+
+	void Start()
+	{
+		my_animator = GetComponent<Animator>();
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-	void Update (){
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		transform.position += new Vector3 (moveHorizontal, 0f, 0f) * Time.deltaTime * speed;
+	void Update()
+	{
+		isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		transform.position += new Vector3(moveHorizontal, 0f, 0f) * Time.deltaTime * speed;
 
-		if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
-			my_animator.SetTrigger ("IsJumping");
+		if (Input.GetKeyDown(KeyCode.Space) && isTouchingGround)
+		{
+			my_animator.SetTrigger("IsJumping");
 			rb.velocity = Vector2.up * jumpVelocity;
-			isGrounded = false;
-	}
-		else if (moveHorizontal > 0) {
-			my_animator.SetTrigger ("IsWalking");
-			transform.localScale = new Vector3 (5f,5f,5f);
-		
 		}
-		else if(moveHorizontal < 0){
-			my_animator.SetTrigger ("IsWalking");
-			transform.localScale = new Vector3 (-5f,5f,5f);
-		} 
-
-		else {
-			my_animator.SetTrigger ("IsIdle");
-		}
-
-	}
-
-	void OnCollisionEnter2D(Collision2D other){
-		if (other.collider.CompareTag ("Ground")) {
-			isGrounded = true;
-
+		else if (moveHorizontal > 0)
+		{
+			my_animator.SetTrigger("IsWalking");
+			transform.localScale = new Vector3(5f, 5f, 5f);
 
 		}
+		else if (moveHorizontal < 0)
+		{
+			my_animator.SetTrigger("IsWalking");
+			transform.localScale = new Vector3(-5f, 5f, 5f);
+		}
+
+		else
+		{
+			my_animator.SetTrigger("IsIdle");
+		}
+
 	}
+
 }
+
