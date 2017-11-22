1a6b5aa961a01c4c3dc2d7f6a2ef6450fc0e329c
trd16 <31514871+trd16@users.noreply.github.com>
2017-11-22 16:27:03 -0500

    pickups

--- Assignment/Assets/Scripts/MainMenuScript.cs
+++ Assignment/Assets/Scripts/MainMenuScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class MainMenuScript : MonoBehaviour {
 
     public GameObject NewGameButton;
     public GameObject OptionsButton;
