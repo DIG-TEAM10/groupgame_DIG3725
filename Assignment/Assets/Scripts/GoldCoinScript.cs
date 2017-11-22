13ee313ef2832b7395ad065d0750dfe80bcd44c7
trd16 <31514871+trd16@users.noreply.github.com>
2017-11-22 17:09:40 -0500

    scene selection buttons and pickups

--- Assignment/Assets/Scripts/GoldCoinScript.cs
+++ Assignment/Assets/Scripts/GoldCoinScript.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class GoldCoinScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("goldcoinpickuptag"))
+        {
+            //... then set the other object we just collided with to inactive.
+            other.gameObject.SetActive(false);
+
+        }
+
+
+    }
 }
