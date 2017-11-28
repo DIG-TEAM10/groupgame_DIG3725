166b46faa9a69e1ab789798cca7953dc9f9fe0d7
Thomas-Field <31514428+Thomas-Field@users.noreply.github.com>
2017-11-28 14:04:14 -0500

    Created Porcupine Enemy and Spike Instantiator

---
+++ Assignment/Assets/Scripts/FlyingSpikeScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FlyingSpikeScript : MonoBehaviour
+{
+
+	private PlayerScript player;
+	public float attacktime = 0.5f;
+	public int damage = 10;
+	Animator a;
+	public Slider pHealth;
+	public PlayerHealth ph;
+	WolfHealth wh;
+	float timer;
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+		ph = player.GetComponent<PlayerHealth>();
+		wh = GetComponent<WolfHealth>();
+		a = GetComponent<Animator>();
+
+	}
+
+	void OnTriggerEnter2D(Collision2D other)
+	{
+
+		print("check3");
+		if (other.collider.CompareTag("Player"))
+		{
+			ph.takeDamage(damage);
+		}
+
+		StartCoroutine(player.Knockback(0.02f, 150, player.transform.position));
+
+	}
+
+}
