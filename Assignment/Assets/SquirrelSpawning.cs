6bea14b7cf8de8a473e216e1b0f490fdb3c25585
trd16 <31514871+trd16@users.noreply.github.com>
2017-11-30 12:53:46 -0500

    acorns

---
+++ Assignment/Assets/SquirrelSpawning.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirrelSpawning : MonoBehaviour {
+
+    public GameObject acorn;
+    int counter = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (counter > 60)
+        {
+            GameObject newacorn = Instantiate(acorn, transform.position, Quaternion.identity);
+            counter = 0;
+            newacorn.GetComponent<Rigidbody2D>().velocity = new Vector2(-5f, 0);
+        }
+
+        counter++;
+
+    }
+}
