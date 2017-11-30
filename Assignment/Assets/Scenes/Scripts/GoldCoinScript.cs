31f2a06b5043fcf1f3b6a7a0bc9bfba3e05fb18d
trd16 <31514871+trd16@users.noreply.github.com>
2017-11-30 12:21:46 -0500

    shredder

---
+++ Assignment/Assets/Scenes/Scripts/GoldCoinScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldCoinScript : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("goldcoinpickuptag"))
+        {
+            //... then set the other object we just collided with to inactive.
+            other.gameObject.SetActive(false);
+
+        }
+
+
+    }
+}
---
+++ Assignment/Assets/Scenes/Scripts/PlayerHealth.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+	public int fullhealth = 100;
+	public int currenthealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public float damageSpeed = 5f;
+	public Color flashColor = new Color(1f, 0f, 0.1f);
+
+
+	Animator a;
+	AudioSource aud;
+	bool isDead;
+	bool isDamaged;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		a = GetComponent<Animator>();
+		aud = GetComponent<AudioSource>();
+		currenthealth = fullhealth;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void takeDamage(int amount)
+	{
+		isDamaged = true;
+
+		currenthealth -= amount;
+
+		healthSlider.value = currenthealth;
+
+		print(currenthealth);
+
+		if (currenthealth <= 0 && !isDead)
+		{
+			Death();
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		print("hello");
+
+		if (other.gameObject.CompareTag("redheartpickuptag"))
+		{
+			currenthealth += 10;
+			healthSlider.value = currenthealth;
+
+			print("healthup");
+		}
+
+	}
+void Death()
+	{
+			isDead = true;
+
+			a.SetTrigger("Die");
+
+		}
+}
