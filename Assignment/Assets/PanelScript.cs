9fff3d4373c494d784ef18167de2e98d96c944f3
trd16 <31514871+trd16@users.noreply.github.com>
2017-12-02 16:20:17 -0500

    popupwork

---
+++ Assignment/Assets/PanelScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PanelScript : MonoBehaviour
+{
+    private GameObject popUp;
+
+    // Use this for initialization
+    void Start()
+    {
+        popUp = GameObject.Find("Panel");
+        popUp.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            popUp.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
