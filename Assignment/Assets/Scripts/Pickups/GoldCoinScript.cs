5dcd720863b59b6cb30c0078968c231af38b65a8
trd16 <31514871+trd16@users.noreply.github.com>
2017-12-03 14:13:00 -0500

    goldcoincollection

--- Assignment/Assets/Scripts/Pickups/GoldCoinScript.cs
+++ Assignment/Assets/Scripts/Pickups/GoldCoinScript.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class GoldCoinScript : MonoBehaviour {
 
-    void OnTriggerEnter2D(Collider2D other)
+    public AudioSource audioSource;
+    private AudioClip coinsparkle;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
-        if (other.gameObject.CompareTag("goldcoinpickuptag"))
-        {
-            //... then set the other object we just collided with to inactive.
-            other.gameObject.SetActive(false);
-
-        }
-
-		else if (other.gameObject.CompareTag("NextLevel"))
-        {
-            //... then set the other object we just collided with to inactive.
-            other.gameObject.SetActive(false);
-
-        }
-
+        audioSource.PlayOneShot(coinsparkle);
     }
-}
+}   
