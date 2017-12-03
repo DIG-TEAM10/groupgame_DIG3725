f72d45bbfb0d4b2da28f61a7c75183e231ec9df3
Thomas-Field <31514428+Thomas-Field@users.noreply.github.com>
2017-12-03 13:18:13 -0500

    Bird Movement

---
+++ Assignment/Assets/Scripts/Enemies/BirdScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+	Vector3 movement;
+	SpriteRenderer mySpriteRenderer;
+
+	private void Awake()
+	{
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+
+	void Start()
+	{
+
+		movement = new Vector3(.1f, 0f);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		transform.position += movement;
+
+
+		if (transform.position.x >= 7.4)
+		{
+			movement = new Vector3(-.1f, 0f);
+			mySpriteRenderer.flipX = false;
+		}
+
+		else if (transform.position.x <= -6.5)
+		{
+			movement = new Vector3(.1f, 0f);
+			mySpriteRenderer.flipX = true;
+		}
+
+	}
+}
+	
