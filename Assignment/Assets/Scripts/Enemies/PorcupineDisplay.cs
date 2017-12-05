d698e03c612af8969043d34fa957e85e6f0e9872
Thomas-Field <31514428+Thomas-Field@users.noreply.github.com>
2017-12-05 13:48:53 -0500

    CutScene

---
+++ Assignment/Assets/Scripts/Enemies/PorcupineDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorcupineDisplay : MonoBehaviour
+{
+	Vector3 movement;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+		movement = new Vector3(0f, .1f);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		transform.position += movement;
+
+
+		if (transform.position.y >= -1.2)
+		{
+			movement = new Vector3(0f, -.1f);
+		}
+
+		else if (transform.position.y <= -3.24)
+		{
+			movement = new Vector3(0f, .1f);
+		}
+
+	}
+}
+
