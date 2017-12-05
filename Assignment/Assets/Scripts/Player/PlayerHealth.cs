9950af2eaf1cf99319e1951604a243566f2b6eac
Thomas-Field <31514428+Thomas-Field@users.noreply.github.com>
2017-12-05 11:30:00 -0500

    Recover

--- Assignment/Assets/Scripts/Player/PlayerHealth.cs
+++ Assignment/Assets/Scripts/Player/PlayerHealth.cs
@@ -17,7 +17,7 @@ public class PlayerHealth : MonoBehaviour
 	public float damageSpeed = 5f;
 	public Color flashColor = new Color(1f, 0f, 0.1f);
 public float WaitTime = 1.0f;
-	GameObject player;
+
 
 	Animator a;
 	AudioSource aud;
